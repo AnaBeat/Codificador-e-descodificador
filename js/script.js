946b7da3a191be6595215f90b002c68f8089aa24
AnaBeat <abeatrizbcosta@gmail.com>
2022-01-26 20:02:00 -0300

    Algumas mudanças

--- js/script.js
+++ js/script.js
@@ -1,31 +1,7 @@
-
-/* Regras Codificador:
-"e" é convertido para "enter"
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação
-*/
-
-/* Regras Decodificador:
-"enter" é convertido para "e"
-"imes" é convertido para "i"
-"ai" é convertido para "a"
-"ober" é convertido para "o"
-"ufat" é convertido para "u"
-Apenas letras minúsculas
-Não permite acentuação
-*/
-
 //Variáveis
 var input = document.querySelector('#input-texto');
 input.focus();
 var cripto = document.querySelector('#btn-cripto');
-cripto.addEventListener('submit', function( event ) {
-            event.preventDefault();
-        });
 var descripto = document.querySelector('#btn-descripto');
 var copiar = document.querySelector('#btn-copy');
 var output = document.querySelector('#msg');
