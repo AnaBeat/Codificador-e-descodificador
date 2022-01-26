bdbcd9a2d1527236eed718bdafaec39fe11e942d
AnaBeat <abeatrizbcosta@gmail.com>
2022-01-26 17:39:31 -0300

    Erros corrigidos

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 
-/* Regras Codificador: 
-"e" é convertido para "enter" 
+/* Regras Codificador:
+"e" é convertido para "enter"
 "i" é convertido para "imes"
 "a" é convertido para "ai"
 "o" é convertido para "ober"
 "u" é convertido para "ufat"
 Apenas letras minúsculas
-Não permite acentuação   
+Não permite acentuação
 */
 
-/* Regras Decodificador: 
-"enter" é convertido para "e" 
+/* Regras Decodificador:
+"enter" é convertido para "e"
 "imes" é convertido para "i"
 "ai" é convertido para "a"
 "ober" é convertido para "o"
 "ufat" é convertido para "u"
 Apenas letras minúsculas
-Não permite acentuação     
+Não permite acentuação
 */
+
+//Variáveis
+var input = document.querySelector('#text.input');
+var cripto = document.querySelector('#btn-cripto');
+var descripto = document.querySelector('#btn-descripto');
